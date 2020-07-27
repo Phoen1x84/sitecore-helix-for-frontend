89c10accdcaf013846eb4a9157a739661d43b2f3
Matthew <matthew.neil84@gmail.com>
2020-07-27 22:11:02 +0100

    testing website and lerna

---
+++ src/Project/Commercial.Website/code/webpack.config.js
@@ -0,0 +1,10 @@
+const path = require('path');
+
+module.exports = {
+    mode: 'development',
+    entry: './src/app/app.js',
+    output: {
+        path: path.resolve(__dirname, './dist'),
+        filename: 'commercial.bundle.js'
+    }
+};
